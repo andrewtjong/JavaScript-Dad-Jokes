80f17f886a4ce743a40131dd6232a3d7e474fafd
Andrew Tjong <72036357+andrewtjong@users.noreply.github.com>
2021-10-13 13:41:07 +0200

    insert headers

--- jokes.js
+++ jokes.js
@@ -11,3 +11,16 @@ const buttonText = [
   'please stop',
   'that was the worst one',
 ];
+
+async function fetchJoke() {
+  const response = await fetch("https://icanhazdadjoke.com", {
+    headers: {
+      Accept: 'application/json',
+    },
+  });
+
+  const joke = await response.json()
+  console.log(joke)
+}
+
+fetchJoke()
